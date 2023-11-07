e350f9d58cfebcfa75d83ba372bb7c482433403d
dani3571 <ccd2024081@est.univalle.edu>
2023-11-07 09:37:09 -0400

    https

--- src/app/utils/api/baseUrl.tsx
+++ src/app/utils/api/baseUrl.tsx
@@ -2,7 +2,7 @@ interface IApi {
   baseUrl: string;
 }
 const URL: IApi = {
-  baseUrl: "http://www.quotes.somee.com/api/",
+  baseUrl: "https://www.quotes.somee.com/api/",
   //baseUrl: "https://localhost:7040/api/",
 };
 
